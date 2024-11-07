e3a350cc62cbb3a0c698d936a73daf02bbf6f500
Kateryna Bilokur <kotiku@gmail.com>
2024-11-07 16:04:50 +1100

    1092. Shortest Common Supersequence

--- script-adv.js
+++ script-adv.js
@@ -4214,3 +4214,141 @@ import {
 // console.log(minDistance(word11, word21)); //5
 // console.log(minDistance(word12, word22)); //2
 // console.log(minDistance(word13, word23)); //3
+
+// -----  -----
+// ----- Neetcode, LeetCode 1092. Shortest Common Supersequence  -----
+// -----  -----
+
+//output 'cabac'
+const str1 = "abac";
+const str2 = "cab";
+
+const str21 = "bbabacaa";
+const str11 = "cccababab";
+
+const str12 = "aaaaaaaa"; //aaaaaaaa
+const str22 = "aaaaaaaa";
+
+const str23 = "bbbaaaba"; //expected bbbaaababbb bbbabaabbba
+const str13 = "bbababbb";
+
+const str14 =
+  "bcaaacbbbcbdcaddadcacbdddcdcccdadadcbabaccbccdcdcbcaccacbbdcbabb";
+const str24 =
+  "dddbbdcbccaccbababaacbcbacdddcdabadcacddbacadabdabcdbaaabaccbdaa";
+
+function shortestCommonSupersequence(str2, str1) {
+  // let prevRow = new Array(str1.length + 1).fill(0);
+  // let longestSubseq = new Array(str2.length).fill(-1);
+  // let curMaxCommonChars = 0;
+
+  // for (let i = 0; i < str1.length; i++) {
+  //   let curRow = new Array(str2.length + 1).fill(0);
+  //   for (let j = 0; j < str2.length; j++) {
+  //     if (str1[i] === str2[j]) {
+  //       curRow[j + 1] = 1 + prevRow[j];
+
+  //       //Saving best chars for common sequence
+  //       if (curRow[j + 1] > curMaxCommonChars) {
+  //         curMaxCommonChars = curRow[j + 1];
+  //         if (longestSubseq[j] === -1 || longestSubseq[j] > j)
+  //           longestSubseq[j] = curRow[j + 1];
+  //       }
+  //     } else {
+  //       curRow[j + 1] = Math.max(prevRow[j + 1], curRow[j]);
+  //     }
+  //   }
+  //   prevRow = curRow;
+  //   console.log(prevRow);
+  // }
+
+  // //compose common sub-sequence array
+  // // const longArr = new Array(prevRow[str2.length]);
+  // const longArr = new Array();
+  // for (let i = 0; i < longestSubseq.length; i++) {
+  //   if (longestSubseq[i] !== -1) {
+  //     longArr.push(str2[i]);
+  //     // longArr[longestSubseq[i] - 1] = str2[longestSubseq[i] - 1];
+  //   }
+  // }
+
+  // console.log("-----");
+  // console.log(longestSubseq);
+  // console.log(longArr);
+  //return prevRow[str2.length];
+
+  //WAY 2 - WORKING algorithm that gives TLE. Fix the one above to get faster solution
+  //1. Find the longest common subsequence
+  let prevRow = new Array(str2.length + 1).fill().map(() => new Array());
+
+  for (let i = 0; i < str1.length; i++) {
+    let curRow = new Array(str2.length + 1).fill().map(() => new Array());
+
+    for (let j = 0; j < str2.length; j++) {
+      if (str1[i] === str2[j]) {
+        if (prevRow[j].length === 0) {
+          curRow[j + 1].push(str1[i]);
+        } else {
+          let elements = [...prevRow[j]];
+          for (let y = 0; y < elements.length; y++) {
+            elements[y] += str1[i];
+          }
+          curRow[j + 1] = Array.from(new Set(elements));
+        }
+      } else {
+        if (prevRow[j + 1].length === 0) {
+          curRow[j + 1] = [...curRow[j]];
+        } else if (curRow[j].length === 0) {
+          curRow[j + 1] = [...prevRow[j + 1]];
+        } else {
+          //keep all valid subsequences
+          curRow[j + 1].push(...prevRow[j + 1]);
+          curRow[j + 1].push(...curRow[j]);
+          curRow[j + 1] = Array.from(new Set(curRow[j + 1]));
+        }
+      }
+    }
+    prevRow = curRow;
+  }
+  console.log(prevRow[str2.length]);
+
+  const longest = prevRow[str2.length].reduce(function (a, b) {
+    return a.length > b.length ? a : b;
+  });
+  console.log(longest);
+
+  //2. Run through both strings and add common letters once but unique subsets separatelly
+  let res = "";
+  let longArr = longest.split("");
+  let str1Arr = str1.split("");
+  let str2Arr = str2.split("");
+
+  longArr.forEach((charCommon) => {
+    let c1 = 0;
+    let c2 = 0;
+
+    for (let i = 0; i < str1Arr.length; i++) {
+      if (str1Arr[i] !== charCommon) {
+        c1++;
+        res = res.concat(str1Arr[i]);
+      } else break;
+    }
+    for (let i = 0; i < str2Arr.length; i++) {
+      if (str2Arr[i] !== charCommon) {
+        c2++;
+        res = res.concat(str2Arr[i]);
+      } else break;
+    }
+
+    res = res.concat(charCommon);
+    str1Arr.splice(0, c1 + 1);
+    str2Arr.splice(0, c2 + 1);
+  });
+
+  res = res.concat(str1Arr.join(""));
+  res = res.concat(str2Arr.join(""));
+
+  return res;
+}
+
+console.log(shortestCommonSupersequence(str14, str24));
